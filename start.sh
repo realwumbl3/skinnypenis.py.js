903b36b924a7135b07118fd4d82eee2364f90679
realwumbl3 <wumbl3@gmail.com>
2024-03-16 21:34:11 -0500

    requirements fix

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-source pyvenv/bin/activate
+source venv/bin/activate
 
 python3.9 -m gunicorn -k eventlet -w 1 app:app --bind unix:"/tmp/thingsai.B.sock" --error-logfile ./log.log --log-level debug --capture-output --reload 

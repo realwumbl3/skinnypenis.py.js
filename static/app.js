6e51037bf9b47d5483fd282c6df776fed6509b7f
realwumbl3 <wumbl3@gmail.com>
2024-03-17 23:49:35 -0500

    ayyy

--- static/app.js
+++ static/app.js
@@ -1,44 +1,38 @@
 import { io } from 'socket.io-client';
 import zyX, { html, css, zyXArray, zyXDomArray } from 'zyX';
 
+await css`url(/static/css.css)`;
+
 const socketio = io('/socket.io');
 
 socketio.on('connect', () => {
-    console.log('%c[socketio] Connected to server', 'color: lime');
-    socketio.emit('enter', { room: "root" });
+	console.log('%c[socketio] Connected to server', 'color: lime');
+	socketio.emit('enter', { room: "root" });
 });
 
 socketio.on('status', (data) => console.log("[socketio] status: ", data));
 
 const messages = new zyXArray(...data.latest_20);
 
-socketio.on('chat', (message) => {
-    messages.push(message)
-});
+socketio.on('chat', (message) => messages.push(message));
 
-const messagesDom = new zyXDomArray(messages, (message) => html`
-    <div class=message>
+const messagesDom = new zyXDomArray(messages, (message) => html`	
+	<div class=message>
         <div class=username>${message.user} - </div>
         <div>${message.content}</div>
     </div>
 `, { classList: ['messages'], range: -10 });
 
-const input = html`
-    <input type="text" this="input" id="input" placeholder="Enter your message">
-`.pass(({ input } = {}) => {
-    input.addEventListener('keyup', (e) => {
-        if (e.key === 'Enter') {
-            if (e.target.value.trim() === '') return;
-            socketio.emit('chat', { content: e.target.value });
-            e.target.value = '';
-        }
-    });
-})
-
-await css`url(/static/css.css)`;
+function handleKeyUp(e) {
+	if (e.key === 'Enter') {
+		if (e.target.value.trim() === '') return;
+		socketio.emit('chat', { content: e.target.value });
+		e.target.value = '';
+	}
+}
 
 html`
     <h1>Chat Lite.</h1>
     ${messagesDom}
-    ${input}
+    <input type="text" this="input" id="input" placeholder="Enter your message" zyx-keyup="${handleKeyUp}">
 `.appendTo(document.body);
